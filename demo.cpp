f5dd527434e600d77328b571a6ed6b568999420c
shashankskds <35990401+shashankskds@users.noreply.github.com>
2018-02-07 23:20:38 +0530

    Add files via upload
    basic calculater

---
+++ demo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+   int a;
+   int b;
+   int sum;
+
+ cout << " enter a number bro \n";
+ cin >> a;
+ 
+ cout << "enter another number bro \n";
+ cin >>b;
+
+  sum = a + b;
+  cout << "the sum of those numbrer is" << sum << endl;
+
+  return 0; 
+
+}
