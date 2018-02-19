9560a5f181711cc8241ade65ceae3cbc53650dcf
shashankskds <35990401+shashankskds@users.noreply.github.com>
2018-02-20 01:23:30 +0530

    Add files via upload
    WEEK 1

---
+++ 4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{                
+	int x , ReversedNumber = 0 , lastdigit;
+                                
+	cout << " enter a number " << endl;
+	cin >> x;
+
+	while ( 0 != x) {
+	           lastdigit = x % 10;                             
+		   ReversedNumber = ReversedNumber* 10 + lastdigit;
+
+		   x = x/10;
+	}
+          cout << " Reversed no... " << ReversedNumber << endl;
+  return 0;
+}
+
+
+
+
+                                                                            
+
