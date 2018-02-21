88d3937e108d13a781f927ec58155e9decb82d15
shashank <shashank.skds@gmail.com>
2018-02-21 23:12:24 +0530

    week 2 completed

---
+++ magic.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{       
+	
+	int x;
+       
+      cout << " Enter a number " << endl;
+      cin >> x;
+                 
+        if(x/100){
+		cout << " Magic " << endl;
+	} else { 
+		cout << " Error " << endl;
+	}
+
+	return 0;
+}
+
---
+++ twoex.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+            int x;
+            int y = 0;
+
+            cout << " Enter a number " << endl;
+	    cin >> x;
+
+	    while (y <= x){
+                       cout << y << endl;
+                                  y = y + 1;}
+
+
+	               
+}
+                                              
